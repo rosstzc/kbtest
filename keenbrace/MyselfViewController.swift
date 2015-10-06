4e567010588d6b10a2814c801ad95658b8c52a45
michaeltam <michaeltam@michaeltamdemacbook-pro.local>
2015-10-06 15:15:12 +0800

    for github

--- keenbrace/MyselfViewController.swift
+++ keenbrace/MyselfViewController.swift
@@ -18,6 +18,7 @@ class MyselfViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
     
